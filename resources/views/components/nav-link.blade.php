00199d0efb13099a8b4bdec999d1c40c894c475b
Hamzaaladaadgithab <aladaada5105390@gmail.com>
2025-09-16 18:26:01 +0300

    - Created Post model and migrations to establish the posts table structure. - Added an `author` column to the posts table. - Introduced a reusable `nav-link` Blade component to handle active states. - Refactored the main layout to use the new `nav-link` component, cleaning up the navigation menu.

---
+++ resources/views/components/nav-link.blade.php
@@ -0,0 +1,16 @@
+@props([
+    'active' => false,
+    'href' => '#'
+])
+
+
+@php
+$current = "bg-gray-950/50 text-white";
+$default = "text-gray-300 hover:bg-white/5 hover:text-white";
+@endphp
+
+<a href="{{$href}}" class="rounded-md  px-3 py-2 text-sm font-medium {{ $active ?  $current:$default}}" {{$attributes}} >
+
+    {{ $slot}}
+
+</a>
